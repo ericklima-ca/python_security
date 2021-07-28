0bafc731320dadec2ef6b727167c55aa17e700ac
ericklima-ca <73451993+ericklima-ca@users.noreply.github.com>
2021-07-27 21:32:17 -0400

    Update web_scrapping.py

--- web_scrapping/web_scrapping.py
+++ web_scrapping/web_scrapping.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests as req
 
-site = req.get('https://climatempo.com.br').content
+site = req.get('https://www.climatempo.com.br/previsao-do-tempo/cidade/25').content
 
 soup = BeautifulSoup(site, 'html.parser')
 
-temp = soup.prettify()
-
-print(temp)
+cidade = soup.find('h1', class_="-bold -font-18 -dark-blue _margin-r-10 _margin-b-sm-5").text
+temp_min = soup.find(id='min-temp-1')
+temp_max = soup.find(id='max-temp-1')
+print(cidade, temp_min.string, temp_max.string)
